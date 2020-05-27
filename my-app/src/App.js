014cad7b14618d61f390984f26881cd10680b342
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-05-26 22:00:09 -0500

    My app did a thing, neat

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+//uses class to allow us to access a state
+class App extends Component {
+  constructor(){
+    super(); //calls constructor on the classApp
+  this.state = {
+    string: 'Hello gorgeous'//The is the string that I just input
+  };
+  };
+  render() {//render returns any HTML you want!  
+    return(
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: 'You look fear in the eye and approach it openly' })}>Text Changer</button>
+        </header>
+        </div>
+    )}}
 export default App;
